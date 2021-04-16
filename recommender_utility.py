d6064bdb6f8da435704357898e51f59a3c668cde
sdds7667 <inplmd@gmail.com>
2021-04-16 19:18:02 +0300

    Normalization with a mask

--- recommender_utility.py
+++ recommender_utility.py
@@ -1,10 +1,15 @@
 import numpy as np
 
+np.set_printoptions(linewidth=150)
+
 
 def normalize_user_ratings(utility_matrix: np.array) -> np.array:
     """
-    Normalizes the ratings for all the users
+    Normalizes the ratings for the cosine similarity by subtracting the average per user from all
+    the non-zero entries of that user
     :param utility_matrix: the utility matrix which has data
     :return:
     """
-    return np.linalg.norm(utility_matrix, axis=1)
+    t = np.transpose
+    return t(t(utility_matrix) - (np.sum(utility_matrix, axis=1) / np.count_nonzero(utility_matrix, axis=1))) \
+           * np.where(utility_matrix > 0, 1, 0)
--- test_data.py
+++ test_data.py
@@ -1,7 +1,7 @@
 from recommender_utility import normalize_user_ratings
 import numpy as np
 
-test_ratings = np.matrix([
+test_ratings = np.array([
         [4, 0, 0, 5, 1, 0, 0],
         [5, 5, 4, 0, 0, 0, 0],
         [0, 0, 0, 2, 4, 5, 0],
